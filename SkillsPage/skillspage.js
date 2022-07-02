d57b8fcd70336cf5b7e6e124d301295648df1bd6
alexismcsherry22 <alexismcsherry22@gmail.com>
2022-07-02 14:12:31 +0930

    skill page finally works

--- SkillsPage/skillspage.js
+++ SkillsPage/skillspage.js
@@ -1,5 +1,31 @@
-function skillTextSwitch() 
+function skillTextSwitch(id) 
 {
-  const popup = document.getElementById("pop-up");
-  popup.classList.toggle("show");
+  const header = document.getElementById("skillHeader");
+  const description = document.getElementById("skillDesc");
+  const example = document.getElementById("skillExample");
+
+  if (id == "skillOne")
+  {
+    header.innerHTML = "Skill One";
+    description.innerHTML = "Skill One Description.";
+    example.innerHTML = "Skill One Example.";
+  }
+  else if (id == "skillTwo")
+  {
+    header.innerHTML = "Skill Two";
+    description.innerHTML = "Skill Two Description.";
+    example.innerHTML = "Skill Two Example.";
+  }
+  else if (id == "skillThree")
+  {
+    header.innerHTML = "Skill Three";
+    description.innerHTML = "Skill Three Description.";
+    example.innerHTML = "Skill Three Example.";
+  }
+  else //no id found
+  {
+    header.innerHTML = "Skill Doesn't Exist";
+    description.innerHTML = "Nothing to see here.";
+    example.innerHTML = "Nothing to see here.";
+  }
 }
